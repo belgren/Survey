16ccb72e2dbef5d4ab8ac31a3c5e0861f1ba8e45
belgrencc <b_elgren@coloradocollege.edu>
2018-04-11 17:16:40 -0600

    JavaDoc for yesNoQuestion

--- src/YesNoQuestion.java
+++ src/YesNoQuestion.java
@@ -1,5 +1,14 @@
-
+/**
+ * Concrete subclass of question defining the yes/no question type.
+ * Contains a process answers method, and an ArrayList of answer objects
+ * that will be filled in when the survey is conducted.
+ */
+import java.util.ArrayList;
 public class YesNoQuestion implements Question{
+	
+	public YesNoQuestion() {
+		ArrayList<Answer> answers;
+	}
 	public void processAnswers() {
 		 
 	}
