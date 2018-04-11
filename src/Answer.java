b6a2fe7a0959177b168a9abe11a1d7daccced06b
belgrencc <b_elgren@coloradocollege.edu>
2018-04-11 17:26:20 -0600

    Created EmailReader

--- src/Answer.java
+++ src/Answer.java
@@ -1,4 +1,15 @@
-
+/**
+ * Class to define answer objects. Answers can be created with a string containing
+ * the text of the answer.  Class contains getters and setters.
+ * @author belgren
+ *
+ */
 public class Answer {
 
+	private String answerText;
+	
+	public Answer(String answerText) {
+		this.answerText = answerText;
+	}
+	
 }
---
+++ src/EmailReader.java
@@ -0,0 +1,18 @@
+/**
+ * Class to read in the survey taker's email responses, and divide them 
+ * into individual answer objects.
+ * @author belgren
+ *
+ */
+public class EmailReader {
+
+	/**
+	 * Eventually this should take in whatever we get from the email
+	 * and return the string of answers
+	 * @return
+	 */
+	public void parseEmail() {
+		
+	}
+
+}
