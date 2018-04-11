1f5c2118dfe47f4c409f8e2068fa98ce2fbbdb67
belgrencc <b_elgren@coloradocollege.edu>
2018-04-11 16:34:03 -0600

    Make question interface

--- src/Question.java
+++ src/Question.java
@@ -1,13 +1,5 @@
 
-public class Question {
+public interface Question {
 	public static void main(String[] args) {
-		System.out.println("Bye");
-		
-		System.out.println("Hello World");
-		System.out.println("Test");
-		System.out.println("3");
-		System.out.println("Jordan New Line");
-		System.out.println("Darryl commited");
-		 
 	}
 }
