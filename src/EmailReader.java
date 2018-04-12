03946c95754797db3356ea26a7329e5b1dba3040
belgrencc <b_elgren@coloradocollege.edu>
2018-04-11 18:00:11 -0600

    JavaDoc updates

--- src/EmailReader.java
+++ src/EmailReader.java
@@ -1,18 +1,6 @@
 /**
- * Class to read in the survey taker's email responses, and divide them 
- * into individual answer objects.
- * @author belgren
- *
+ * THIS CLASS SHOULD BE DELETED
  */
 public class EmailReader {
-
-	/**
-	 * Eventually this should take in whatever we get from the email
-	 * and return the string of answers
-	 * @return
-	 */
-	public void parseEmail() {
-		
-	}
 
 }
--- src/Survey.java
+++ src/Survey.java
@@ -1,4 +1,14 @@
 
 public class Survey {
-
+	
+	/**
+	 * Read through all emails for a given survey and split each line in each email
+	 * into individual answers. Create answer objects for each of them, and put all
+	 * of the answers into an array list corresponding to one survey taker's set of answers.
+	 * Each of these arrays will be added to another array that will hold all responses and
+	 * be returned by the method.
+	 */
+	public void parseEmails() {
+		
+	}
 }
