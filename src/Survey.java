9445b2d584909fd515905c91d5e7dc3d949a3173
belgrencc <b_elgren@coloradocollege.edu>
2018-04-11 18:13:10 -0600

    changes to survey

--- src/Survey.java
+++ src/Survey.java
@@ -1,6 +1,13 @@
 
 public class Survey {
 	
+	private Question question;
+	
+	
+	public void addYesNoQuestion(String questionText) {
+		question = new YesNoQuestion();
+	}
+	
 	/**
 	 * Read through all emails for a given survey and split each line in each email
 	 * into individual answers. Create answer objects for each of them, and put all
@@ -8,12 +15,13 @@ public class Survey {
 	 * Each of these arrays will be added to another array that will hold all responses and
 	 * be returned by the method.
 	 */
-	public void addQuestion() {
+	public void parseEmails() {
 		
 	}
 	
-	public void parseEmails() {
-		
+	public void runSurvey() {
+		Survey survey = new Survey();
+		survey.addYesNoQuestion("Yes or No?");
 	}
 	
 	public static void main(String args[]) {
