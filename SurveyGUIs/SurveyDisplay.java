67a21081cc807ee92a080b9fb35818d8436a07a7
Darryl Filmore <darrylfilmore@gmail.com>
2018-04-15 15:01:12 -0600

    More GUI editing

--- SurveyGUIs/SurveyDisplay.java
+++ SurveyGUIs/SurveyDisplay.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
@@ -42,30 +43,27 @@ public class SurveyDisplay {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 300 + (survey.getQuestionList().size() * 90));
+		frame.setBounds(100, 100, 500, (300 + (survey.getQuestionList().size() * 90)));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(0, 0, 500, 678);
-		frame.getContentPane().add(scrollPane);
 		
 		JLabel surveyTitle = new JLabel(survey.getSurveyName());
 		surveyTitle.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		surveyTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		surveyTitle.setBounds(24, 18, 416, 90);
+		surveyTitle.setBounds(24, 15, 416, 90);
 		frame.getContentPane().add(surveyTitle);
 		
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(429, 6, 15, 266);
-		frame.getContentPane().add(scrollBar);
-		
-		int counter = 0;
-		for(QuestionStrategy q : survey.getQuestionList()) {
-			JTextPane textPane = new JTextPane();
-			textPane.setBounds(6, 18 + (counter * 90), 414, 90);
-			frame.getContentPane().add(textPane);
-		}
+//		JScrollPane scrollPane = new JScrollPane();
+//		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+//		scrollPane.setBounds(0, 0, 500, 678);
+//		frame.getContentPane().add(scrollPane);
+//		frame.add(scrollPane);
+//		
+//		int counter = 0;
+//		for(QuestionStrategy q : survey.getQuestionList()) {
+//			JTextPane textPane = new JTextPane();
+//			textPane.setBounds(6, 18 + (counter * 90), 414, 90);
+//			frame.getContentPane().add(textPane);
+//		}
 	}
 }
